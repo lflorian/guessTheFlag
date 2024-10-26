91d9555d26d2d995a5ebdde4e29c64646f43726d
Florian <florian.lammert@icloud.com>
2024-10-26 16:25:51 +0200

    Initial Commit

---
+++ guessTheFlag/guessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  guessTheFlagApp.swift
+//  guessTheFlag
+//
+//  Created by Florian Lammert on 26.10.24.
+//
+
+import SwiftUI
+
+@main
+struct guessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
